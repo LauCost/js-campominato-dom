4ee6311d347ffa047b002a56eea6c5e7fcd707ec
Laurentiu <lcostin40@gmail.com>
2021-10-21 17:29:39 +0200

    smaltito codice js per la creazione della griglia

--- JS/main.js
+++ JS/main.js
@@ -6,64 +6,31 @@ const hard = document.querySelector(".btn-hard");
 
 const container = document.querySelector(".cont")
 
-
 easy.addEventListener(`click`, function () {
     container.innerHTML = ``;
     quadrati = 50;
+    generaGriglia(quadrati);
 
-    for (i = 1; i < quadrati; i++) {
 
-        //Crea un elemento div
-        const divEl = document.createElement("div");
-
-        //Aggiunge la classe quadrato all'elemento appena creato
-        divEl.className = "quadrato"
-
-        //Stampa la griglia nella DOM
-        container.append(divEl)
-
-        //Stampa i numeri dei quadrati nella DOM
-        divEl.innerHTML += `<p class="numero"> ${i} </p>`
-
-        //Funzione che server a colorare un quadrato quando ci si clicca sopra
-        divEl.addEventListener(`click`, function () {
-
-            //Aggiunge il colore celeste all'elemento cliccato
-            this.style.backgroundColor = "cyan"
-        })
-    }
 })
+
+
 
 normal.addEventListener(`click`, function () {
     container.innerHTML = ``;
     quadrati = 82;
+    generaGriglia(quadrati);
 
-    for (i = 1; i < quadrati; i++) {
-
-        //Crea un elemento div
-        const divEl = document.createElement("div");
-
-        //Aggiunge la classe quadrato all'elemento appena creato
-        divEl.className = "quadrato"
-
-        //Stampa la griglia nella DOM
-        container.append(divEl)
-
-        //Stampa i numeri dei quadrati nella DOM
-        divEl.innerHTML += `<p class="numero"> ${i} </p>`
-
-        //Funzione che server a colorare un quadrato quando ci si clicca sopra
-        divEl.addEventListener(`click`, function () {
-
-            //Aggiunge il colore celeste all'elemento cliccato
-            this.style.backgroundColor = "cyan"
-        })
-    }
 })
 
 hard.addEventListener(`click`, function () {
     container.innerHTML = ``;
     quadrati = 101;
+    generaGriglia(quadrati);
+
+})
+
+function generaGriglia(quadrati) {
 
     for (i = 1; i < quadrati; i++) {
 
@@ -77,16 +44,17 @@ hard.addEventListener(`click`, function () {
         container.append(divEl)
 
         //Stampa i numeri dei quadrati nella DOM
-        divEl.innerHTML += `<p class="numero"> ${i} </p>`
+        divEl.innerHTML += ` ${i} `
 
         //Funzione che server a colorare un quadrato quando ci si clicca sopra
         divEl.addEventListener(`click`, function () {
+            let cellNumber = this.innerHTML
+            console.log(cellNumber);
 
             //Aggiunge il colore celeste all'elemento cliccato
             this.style.backgroundColor = "cyan"
         })
     }
-})
 
-
+}
 
